f76cdf1bbe2c58fe16fafe3819754182656e6a6e
Travis Dalhoff <tdalhoff@live.com>
2017-07-19 21:24:44 -0400

    Updated styles.

--- BeerTracker5000/Models/Beer.cs
+++ BeerTracker5000/Models/Beer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,14 @@ namespace BeerTracker5000.Models
     public class Beer
     {
         public int ID { get; set; }
+        [Display(Name = "Beer Name")]
         public string BeerName { get; set; }
+        [Display(Name = "Brewery Name")]
         public string BreweryName { get; set; }
         public string Style { get; set; }
         public decimal Abv { get; set; }
         public int Ibu { get; set; }
-        public int Rating { get; set; }
+        //public int Rating { get; set; }
         public string Notes { get; set; }
     }
 }
